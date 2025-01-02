0d73327d15b521d3d33c2533c62e93e2c2e44893
hd0rable <kanghuijin12@gmail.com>
2025-01-02 23:27:40 +0900

    init

---
+++ src/main/java/Kuitso/demo/KuitsoApplication.java
@@ -0,0 +1,13 @@
+package Kuitso.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuitsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuitsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kuitso/demo/KuitsoApplicationTests.java
@@ -0,0 +1,13 @@
+package Kuitso.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuitsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
