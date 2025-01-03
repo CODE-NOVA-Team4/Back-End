d66d7bd6b9130bf051e1989a16d4a46c3d7946a4
ksg <ksg001227@naver.com>
2025-01-04 00:19:39 +0900

    feat: 이메일 인증 추가

--- src/main/java/Kuitso/demo/service/AuthService.java
+++ src/main/java/Kuitso/demo/service/AuthService.java
@@ -1,17 +1,23 @@
 package Kuitso.demo.service;
 
 import Kuitso.demo.common.exception.UserException;
+import Kuitso.demo.common.response.BaseResponse;
 import Kuitso.demo.domain.User;
 import Kuitso.demo.domain.base.BaseStatus;
-import Kuitso.demo.dto.auth.PostSLogInRequest;
-import Kuitso.demo.dto.auth.PostSLogInResponse;
-import Kuitso.demo.dto.auth.PostSignUpRequest;
+import Kuitso.demo.dto.auth.*;
 import Kuitso.demo.repository.UserRepository;
+import com.univcert.api.UnivCert;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import static Kuitso.demo.common.response.status.BaseExceptionResponseStatus.*;
 import static Kuitso.demo.domain.base.BaseStatus.ACTIVE;
@@ -23,6 +29,8 @@ import static Kuitso.demo.domain.base.BaseStatus.DELETED;
 public class AuthService {
 
     private final UserRepository userRepository;
+//    @Value("{univcert.api.key")
+    private String apiKey = "2a4efd85-c695-4dc9-bffe-f0dab0a15363";
 
     public void signup(PostSignUpRequest postSignUpRequest, HttpServletRequest request) {
 
@@ -32,9 +40,9 @@ public class AuthService {
         String department = postSignUpRequest.getDepartment();
 
         //이미 존재하는 회원 예외처리
-        if(userRepository.existsByEmailAndStatus(email,ACTIVE)){
+        if (userRepository.existsByEmailAndStatus(email, ACTIVE)) {
             throw new UserException(ALREADY_EXIST_USER);
-        }else{
+        } else {
 
             //데이터베이스에 저장
             User user = new User(nickname, email, password, department);
@@ -50,7 +58,7 @@ public class AuthService {
     public void signout(HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
-        if(session != null) {
+        if (session != null) {
             Long userId = (Long) (request.getSession().getAttribute("userId"));
 
             User user = userRepository.findByUserIdAndStatus(userId, ACTIVE)
@@ -61,8 +69,7 @@ public class AuthService {
 
             //유저상태변경
             user.setStatus(DELETED);
-        }
-        else throw new UserException(CANNOT_FOUND_SESSION);
+        } else throw new UserException(CANNOT_FOUND_SESSION);
 
     }
 
@@ -70,15 +77,14 @@ public class AuthService {
         String email = postSLogInRequest.getEmail();
         String password = postSLogInRequest.getPassword();
 
-        User user = userRepository.findByEmailAndStatus(email,ACTIVE)
+        User user = userRepository.findByEmailAndStatus(email, ACTIVE)
                 .orElseThrow(() -> new UserException(CANNOT_FOUND_USER));
 
-        if(user.getPassword().equals(password)){
+        if (user.getPassword().equals(password)) {
             log.info("Login successful");
             HttpSession session = request.getSession();
             session.setAttribute("userId", user.getUserId());
-        }
-        else throw new UserException(LOGIN_FAILED);
+        } else throw new UserException(LOGIN_FAILED);
 
         return new PostSLogInResponse(user.getUserId());
 
@@ -87,11 +93,38 @@ public class AuthService {
     public void logout(HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
-        if(session != null) {
+        if (session != null) {
 
             //세션 무효화
             request.getSession().removeAttribute("userId");
+        } else throw new UserException(CANNOT_FOUND_SESSION);
+    }
+
+    public Boolean sendCode(PostCodeRequest postCodeRequest) throws IOException {
+        String email = postCodeRequest.getEmail();
+        String univName = postCodeRequest.getUnivName();
+        Boolean univCheck = postCodeRequest.getUniv_check();
+        UnivCert.clear(apiKey, email);
+
+        Map<String, Object> result = UnivCert.certify(apiKey, email, univName, univCheck);
+
+        if (!(boolean) result.get("success")) {
+            return true;
+        } else {
+            return false;
         }
-        else throw new UserException(CANNOT_FOUND_SESSION);
+    }
+
+    public PostVerificationResponse verification(PostVerificationRequest postVerificationRequest) throws IOException {
+        String email = postVerificationRequest.getEmail();
+        String univName = postVerificationRequest.getUnivName();
+        int code = Integer.parseInt(postVerificationRequest.getCode());
+        Map<String, Object> result = UnivCert.certifyCode(apiKey, email, univName, code);
+
+        if ((boolean) result.get("success")) {
+            return new PostVerificationResponse(true, univName, email, LocalDateTime.now());
+        } else{
+            return new PostVerificationResponse(false, "", "", LocalDateTime.now());
+        }
     }
 }
