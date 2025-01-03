79ae80d8dc83724dfe69957d9e4b07824c71fa2b
hd0rable <kanghuijin12@gmail.com>
2025-01-03 20:40:19 +0900

    chore : dto 수정

--- src/main/java/Kuitso/demo/dto/GetHomeResponse.java
+++ src/main/java/Kuitso/demo/dto/GetHomeResponse.java
@@ -1,9 +1,12 @@
 package Kuitso.demo.dto;
 
 import Kuitso.demo.domain.Product;
+import Kuitso.demo.dto.product.GetProductSearchResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -14,6 +17,17 @@ public class GetHomeResponse {
      * 카테고리별 조회 response dto
      */
     private String nickname;
-    private List<Product> products;
+    private List<ProductDetail> products;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProductDetail {
+        private long productId;      // 상품 ID
+        private String picture;        // 상품 이미지
+        private String productName;    // 상품명
+        private int price;             // 가격
+        private LocalDateTime updatedAt;
+    }
 
 }
